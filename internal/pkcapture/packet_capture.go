9ef8831253af738dbc52b3753cb488ca711c06bd
AlammobaDar <162420273+alammobaDar@users.noreply.github.com>
2026-06-26 15:58:03 +0800

    fixed the src and dst port bug, where it is not returning the real ports of each packets

--- internal/pkcapture/packet_capture.go
+++ internal/pkcapture/packet_capture.go
@@ -29,17 +29,17 @@ func PacketCapture(device string, snaplen int32, promisc bool, timeout int) erro
 		srcIP, dstIP, srcPort, dstPort, protocol := CaptureFiveTuples(packet)
 
 
-		fmt.Printf("Packet captured at %s, SrcIP: %s DstIP: %s SrcPort: %d DstPort: %d Protocol: %s length: %d bytes\n",
+		fmt.Printf("Packet captured at %s, SrcIP: %s DstIP: %s SrcPort: %s DstPort: %s Protocol: %s length: %d bytes\n",
 		 metadata.Timestamp.Format(time.RFC3339), srcIP, dstIP, srcPort, dstPort, protocol, metadata.Length)	}
 
 	return nil
 }
 
-func CaptureFiveTuples(packet gopacket.Packet) (srcIP, dstIP string, srcPort, dstPort uint16, protocol string) {
+func CaptureFiveTuples(packet gopacket.Packet) (srcIP, dstIP string, srcPort, dstPort string, protocol string) {
 
 	netlayer := packet.NetworkLayer()
 	if netlayer == nil {
-		return "", "", 0, 0, ""
+		return "", "", "0", "0", ""
 	}
 
 	netflow := netlayer.NetworkFlow()
@@ -48,12 +48,12 @@ func CaptureFiveTuples(packet gopacket.Packet) (srcIP, dstIP string, srcPort, ds
 	
 	transportlayer := packet.TransportLayer()
 	if transportlayer == nil {
-		return srcIP, dstIP, 0, 0, ""
+		return srcIP, dstIP, "0", "0", ""
 	}
 
 	transFlow := transportlayer.TransportFlow()
-	srcPort = uint16(transFlow.Src().EndpointType())
-	dstPort = uint16(transFlow.Dst().EndpointType())
+	srcPort = transFlow.Src().String()
+	dstPort = transFlow.Dst().String()
 	protocol = transportlayer.LayerType().String()
 
 	return srcIP, dstIP, srcPort, dstPort, protocol
