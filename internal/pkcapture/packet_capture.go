dba20278c22d1b05b24c8399f4b87f732c4390ee
AlammobaDar <162420273+alammobaDar@users.noreply.github.com>
2026-06-26 15:50:23 +0800

    feat: add function to capture the TransportLayers Ids that will consists the five-tuple of a flow

--- internal/pkcapture/packet_capture.go
+++ internal/pkcapture/packet_capture.go
@@ -26,8 +26,36 @@ func PacketCapture(device string, snaplen int32, promisc bool, timeout int) erro
 	for packet := range packetSource.Packets() {
 		
 		metadata := packet.Metadata()
-		fmt.Printf("Packet captured at %s, length: %d bytes\n", metadata.Timestamp.Format(time.RFC3339), metadata.Length)
-	}
+		srcIP, dstIP, srcPort, dstPort, protocol := CaptureFiveTuples(packet)
+
+
+		fmt.Printf("Packet captured at %s, SrcIP: %s DstIP: %s SrcPort: %d DstPort: %d Protocol: %s length: %d bytes\n",
+		 metadata.Timestamp.Format(time.RFC3339), srcIP, dstIP, srcPort, dstPort, protocol, metadata.Length)	}
 
 	return nil
+}
+
+func CaptureFiveTuples(packet gopacket.Packet) (srcIP, dstIP string, srcPort, dstPort uint16, protocol string) {
+
+	netlayer := packet.NetworkLayer()
+	if netlayer == nil {
+		return "", "", 0, 0, ""
+	}
+
+	netflow := netlayer.NetworkFlow()
+	srcIP = netflow.Src().String()
+	dstIP = netflow.Dst().String()
+	
+	transportlayer := packet.TransportLayer()
+	if transportlayer == nil {
+		return srcIP, dstIP, 0, 0, ""
+	}
+
+	transFlow := transportlayer.TransportFlow()
+	srcPort = uint16(transFlow.Src().EndpointType())
+	dstPort = uint16(transFlow.Dst().EndpointType())
+	protocol = transportlayer.LayerType().String()
+
+	return srcIP, dstIP, srcPort, dstPort, protocol
+
 }
