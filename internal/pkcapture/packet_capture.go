282a6b40e13f477dffaa517dc70c913b1cc51f76
AlammobaDar <162420273+alammobaDar@users.noreply.github.com>
2026-06-26 16:27:00 +0800

    chore: add struct for the flow_ids

--- internal/pkcapture/packet_capture.go
+++ internal/pkcapture/packet_capture.go
@@ -11,6 +11,14 @@ import (
 )
 
 
+type FlowID struct {
+	SrcIP  string
+	DstIP string
+	SrcPort uint16
+	DstPort uint16
+	Protocol string
+}
+
 func PacketCapture(device string, snaplen int32, promisc bool, timeout int) error {
 
 	handle, err := pcap.OpenLive(device, snaplen, promisc, time.Duration(timeout))
@@ -27,43 +35,43 @@ func PacketCapture(device string, snaplen int32, promisc bool, timeout int) erro
 	for packet := range packetSource.Packets() {
 		
 		metadata := packet.Metadata()
-		srcIP, dstIP, srcPort, dstPort, protocol := CaptureFiveTuples(packet)
+		flowID := CaptureFiveTuples(packet)
 
 
 		fmt.Printf("Packet captured at %s, SrcIP: %s DstIP: %s SrcPort: %d DstPort: %d Protocol: %s length: %d bytes\n",
-		 metadata.Timestamp.Format(time.RFC3339), srcIP, dstIP, srcPort, dstPort, protocol, metadata.Length)	}
+		metadata.Timestamp.Format(time.RFC3339), flowID.SrcIP, flowID.DstIP, flowID.SrcPort, flowID.DstPort, flowID.Protocol, metadata.Length)	}
 
 	return nil
 }
 
-func CaptureFiveTuples(packet gopacket.Packet) (srcIP, dstIP string, srcPort, dstPort uint16, protocol string) {
+func CaptureFiveTuples(packet gopacket.Packet) (flowID FlowID) {
 
 	if netlayer := packet.NetworkLayer() ; netlayer != nil {
 		netflow := netlayer.NetworkFlow()
-		srcIP = netflow.Src().String()
-		dstIP = netflow.Dst().String()
+		flowID.SrcIP = netflow.Src().String()
+		flowID.DstIP = netflow.Dst().String()
 	}
 
 	if tcpLayer := packet.Layer(layers.LayerTypeTCP); tcpLayer != nil {
 		tcp, _ := tcpLayer.(*layers.TCP)
-		srcPort := uint16(tcp.SrcPort)
-		dstPort := uint16(tcp.DstPort)
-		protocol = "TCP"
-		return srcIP, dstIP, srcPort, dstPort, protocol
+		flowID.SrcPort = uint16(tcp.SrcPort)
+		flowID.DstPort = uint16(tcp.DstPort)
+		flowID.Protocol = "TCP"
+		return flowID
 	}
 
 	if udpLayer := packet.Layer(layers.LayerTypeUDP); udpLayer != nil {
 		udp, _ := udpLayer.(*layers.UDP)
-		srcPort := uint16(udp.SrcPort)
-		dstPort := uint16(udp.DstPort)
-		protocol = "UDP"
-		return srcIP, dstIP, srcPort, dstPort, protocol
+		flowID.SrcPort = uint16(udp.SrcPort)
+		flowID.DstPort = uint16(udp.DstPort)
+		flowID.Protocol = "UDP"
+		return flowID
 	}
 	
 	if transportLayer := packet.TransportLayer(); transportLayer != nil {
-		protocol = transportLayer.LayerType().String()
+		flowID.Protocol = transportLayer.LayerType().String()
 	}
 
-	return srcIP, dstIP, 0, 0, protocol
+	return flowID
 
 }
